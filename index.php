4792337103d879d45a82648c63065916259fd08f
Ольга Шутылева <99029912+shytuleva24@users.noreply.github.com>
2022-05-31 22:29:17 +0300

    last

--- index.php
+++ index.php
@@ -62,38 +62,38 @@
                 }
 
                 $volume = $_POST['volume'];
-                $tea = 0;
                 $time = 0.48;
                 
                 echo "Ви обрали $water мл води та $sugar ч.л. цукру <br>";
-                echo "<p>Чайник закипів</p>";
                 
                 $sugar /= $water;
                 $cup = 0;
-
-                for ($i = $water; $i >= 50; $i -= 50) {
-                    $cup += 50;
-                    echo ("<p>Налито ".$cup." мл води</p>");
-                    if ($cup == $volume) {
-                        echo ("<p>Кружка повна</p>");
-                        echo ("<p>Кидаємо " .number_format(($sugar * $cup), 1). " ч.л. цукру</p>");
-                        echo ("<p>Опускаємо чайний пакетик на " .(int)($time * $cup). " сек</p>");
-                        echo ("<p>Розмішуємо</p>");
-                        echo ("<strong>Кружка чаю готова</strong>");
-                        if ($i != 50) {
-                            echo ("<p>Беремо наступну кружку</p>");
+                
+                if ($water != 0) {
+                    echo "<p>Чайник закипів</p>";
+                    for ($i = $water; $i >= 50; $i -= 50) {
+                        $cup += 50;
+                        echo ("<p>Налито ".$cup." мл води</p>");
+                        if ($cup == $volume) {
+                            echo ("<p>Кружка повна</p>");
+                            echo ("<p>Кидаємо " .number_format(($sugar * $cup), 1). " ч.л. цукру</p>");
+                            echo ("<p>Опускаємо чайний пакетик на " .(int)($time * $cup). " сек</p>");
+                            echo ("<p>Розмішуємо</p>");
+                            echo ("<strong>Кружка чаю готова</strong>");
+                            if ($i != 50) {
+                                echo ("<p>Беремо наступну кружку</p>");
+                            }
+                            $cup = 0;
+                        } else if ($i == 50) {
+                            echo ("<p>Вода закінчилась</p>");
+                            echo ("<p>Кидаємо " .number_format(($sugar * $cup), 1). " ч.л. цукру</p>");
+                            echo ("<p>Опускаємо чайний пакетик на " .(int)($time * $cup). " сек</p>");
+                            echo ("<p>Розмішуємо</p>");
+                            echo ("<strong>Кружка чаю готова</strong>");
                         }
-                        $cup = 0;
-                    } else if ($i == 50) {
-                        echo ("<p>Вода закінчилась</p>");
-                        echo ("<p>Кидаємо " .number_format(($sugar * $cup), 1). " ч.л. цукру</p>");
-                        echo ("<p>Опускаємо чайний пакетик на " .(int)($time * $cup). " сек</p>");
-                        echo ("<p>Розмішуємо</p>");
-                        echo ("<strong>Кружка чаю готова</strong>");
                     }
+                    echo ("<p>Смачного чаювання!</p>");
                 }
-
-                echo ("<p>Смачного чаювання!</p>");
 
                     
             ?>
